85ad19fce0b1827a8ef77e82fc2ee80403d617a0
Ben Smith <ben@.(none)>
2012-03-21 19:53:00 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreakSEOAffiliateManager::Application.config.secret_token = 'a7706d4f3565bc78d27e67f821c18e8d46aa39ce0d965aa0c101a0e6fddf7d721b66f46f6161828c947374fbf18a2c067846f9eb4f72dbd1f70f15f06f1574cd'
