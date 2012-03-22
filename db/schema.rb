d5cd8502349584831335b96b407db2828d78fb51
Ben Smith <ben@.(none)>
2012-03-22 20:45:02 +0000

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120321221755) do
+ActiveRecord::Schema.define(:version => 20120322201016) do
 
   create_table "affiliates", :force => true do |t|
-    t.string   "name"
-    t.string   "url"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "affiliate_name"
+    t.string   "affiliate_url"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
 end
